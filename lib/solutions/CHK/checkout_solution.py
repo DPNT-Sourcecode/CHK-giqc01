614e23901696c03f199ec2cb569ab026c5d32adc
sbx_user1051 <sbx_user1051@169.254.162.205>
2022-07-28 09:17:49 +0000

    Thu Jul 28 09:17:49 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,7 +9,8 @@ def checkout(skus):
     num_C = skus.count('C')
     num_D = skus.count('D')
     num_E = skus.count('E')
-    if (num_A + num_B + num_C + num_D + num_E) < len(skus):
+    num_F = skus.count('F')
+    if (num_A + num_B + num_C + num_D + num_E + num_F) < len(skus):
         return -1
     else:
         num_A_d = num_A #num_A_d: number of A that can be discounted
@@ -23,12 +24,12 @@ def checkout(skus):
 
         discount_B = num_B//2
 
+        discount_F = num_F//3
+        num_F -= discount_F
+        
         total = 50*num_A - 50*discount_A_1 - 20*discount_A_2  + 30*num_B - 15*discount_B\
-                + 20*num_C + 15*num_D + 40*num_E
+                + 20*num_C + 15*num_D + 40*num_E + 10*num_F
         return total
 
-skus = "EE"
+skus = "FFFF"
 print(checkout(skus))
-
-
-
