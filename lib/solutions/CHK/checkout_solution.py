070cb979b6c93a56488fa002b76496692f1493e8
sbx_user1051 <sbx_user1051@169.254.162.205>
2022-07-28 09:03:10 +0000

    Thu Jul 28 09:03:10 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,26 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    if skus.count('a') >= 1:
-        return -1
-    if skus.count('-') >= 1:
-        return -1
     
     num_A = skus.count('A')
     num_B = skus.count('B')
     num_C = skus.count('C')
     num_D = skus.count('D')
+    if (num_A + num_B + num_C + num_D) < len(skus):
+        return -1
+    else:
 
-    discount_A = num_A//3
+        discount_A = num_A//3
 
-    discount_B = num_B//2
+        discount_B = num_B//2
 
-    total = 50*num_A - 20*discount_A + 30*num_B - 15*discount_B\
-            + 20*num_C + 15*num_D
-    return total
+        total = 50*num_A - 20*discount_A + 30*num_B - 15*discount_B\
+                + 20*num_C + 15*num_D
+        return total
 
-#skus = "A,B,C,A,A"
-#print(checkout(skus))
-
-
-
+skus = "AAa"
+print(checkout(skus))
